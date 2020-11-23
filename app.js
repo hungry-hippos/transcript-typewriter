c8714340a9452a85c289a14b762662ed88553b39
Hungry Hippo <aduca96@gmail.com>
2020-11-22 16:10:59 -0800

    updates

--- app.js
+++ app.js
@@ -7,13 +7,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'));
 
 
-app.get('/home',function(req,res){
-	res.render('home.ejs');
-})
-app.get('/shiftingHome',function(req,res){
-	res.render('shiftingHome.ejs');
-})
-app.get('/anotherHome',function(req,res){
+app.get('/',function(req,res){
 	res.render('anotherHome.ejs');
 })
 
